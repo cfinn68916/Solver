e0eb59f05b6bb51a6d1687017f942948b8f1548b
Colin <cfinn68916@gmail.com>
2023-10-16 19:56:25 -0500

    Add Iterations limit, and configuration inputs
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- src/App.ts
+++ src/App.ts
@@ -7,20 +7,37 @@ function solve() {
     let lside = handleTop(new Tokenizer($('#lside').val()));
     let rside = handleTop(new Tokenizer($('#rside').val()));
     let err = new SubtractAST(lside, rside);
+    let min_guess=parseFloat($('#guess_min').val().toString());
+    let max_guess=parseFloat($('#guess_max').val().toString());
+    let guess_step=parseFloat($('#guess_step').val().toString());
+    let stop_err=parseFloat($('#stopping_error').val().toString());
+    let group_dev=parseFloat($('#group_deviation').val().toString());
+    let max_iter=parseFloat($('#max_iter').val().toString());
+
 
     let solutions: number[] = [];
-    for (let initx = -100; initx < 100; initx += 1) {
+    for (let initx = min_guess; initx < max_guess; initx += guess_step) {
         if (err.getDerVal(initx) != 0) {
             let x = initx;
-            while (Math.abs(err.getVal(x)) > 0.000000000001) {
+            for (let i = 0; i < max_iter; i++) {
                 x = x - (err.getVal(x) / err.getDerVal(x));
+                if(Math.abs(err.getVal(x)) < stop_err || isNaN(x)){
+                    break;
+                }
+            }
+            if(Math.abs(err.getVal(x)) > stop_err || isNaN(x)){
+                continue;
+            }
+
+            if(isNaN(err.getDerVal(x))){
+                continue;
             }
             if (solutions.length == 0) {
                 solutions.push(x);
             } else {
                 let alreadyin = false;
                 for (let sol of solutions) {
-                    if (Math.abs(sol - x) < 0.0001) {
+                    if (Math.abs(sol - x) < group_dev) {
                         alreadyin = true;
                         break;
                     }
